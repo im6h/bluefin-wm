0d46cf4cc0554d2eca72a8e364aab78f862202c0
im6h <danghaiv.18@gmail.com>
2026-08-18 12:32:09 +0700

    feat: add disk space cleanup to build workflow and remove unused SDDM configuration and terminal packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,10 +8,6 @@ log() {
 	echo "=== $* ==="
 }
 
-# if true, sddm will be installed as the display manager.
-# NOTE: NOT FULLY IMPLEMENTED AND UNTESTED, DO NOT USE YET
-USE_SDDM=FALSE
-
 #######################################################################
 # Setup Repositories
 #######################################################################
@@ -28,16 +24,13 @@ COPR_REPOS=(
 	yalter/niri
 	scottames/ghostty
 )
+
 for repo in "${COPR_REPOS[@]}"; do
 	# Try to enable the repo, but don't fail the build if it doesn't support this Fedora version
 	if ! dnf5 -y copr enable "$repo" 2>&1; then
 		log "Warning: Failed to enable COPR repo $repo (may not support Fedora $RELEASE)"
 	fi
 done
-
-# log "Enable terra repositories..."
-# Bazzite disabled this for some reason so lets re-enable it again
-# dnf5 config-manager setopt terra.enabled=1 terra-extras.enabled=1
 
 #######################################################################
 ## Install Packages
@@ -112,25 +105,10 @@ NIRI_PKGS=(
 	wget2
 )
 
-# SDDM not set up properly yet, so this is just a placeholder.
-# For now you'll have to invoke the compositor from the command line.
-SDDM_PACKAGES=()
-if [[ $USE_SDDM == TRUE ]]; then
-	SDDM_PACKAGES=(
-		sddm
-		sddm-breeze
-		sddm-kcm
-		qt6-qt5compat
-	)
-fi
-
 # chrome etc are installed as flatpaks. We generally prefer that
 # for most things with GUIs, and homebrew for CLI apps. This list is
 # only special GUI apps that need to be installed at the system level.
 ADDITIONAL_SYSTEM_APPS=(
-	alacritty
-	kitty
-	kitty-terminfo
 	thunar
 	thunar-volman
 	thunar-archive-plugin
@@ -154,27 +132,6 @@ for repo in "${COPR_REPOS[@]}"; do
 	dnf5 -y copr disable "$repo"
 done
 
-#######################################################################
-### Enable Services
-
-# TODO: these need to be run at first boot, not during image build
-
-# Setting Thunar as the default file manager
-# xdg-mime default thunar.desktop inode/directory
-# xdg-mime default thunar.desktop application/x-wayland-gnome-saved-search
-
-if [[ $USE_SDDM == TRUE ]]; then
-	log "Installing sddm...."
-	for login_manager in lightdm gdm lxdm lxdm-gtk3; do
-		if sudo dnf list installed "$login_manager" &>>/dev/null; then
-			sudo systemctl disable "$login_manager" 2>&1 | tee -a "$LOG"
-		fi
-	done
-	systemctl set-default graphical.target
-	systemctl enable sddm.service
-fi
-
-#######################################################################
 ### Install fcitx5-lotus from GitHub Releases
 
 log "Installing fcitx5-lotus from GitHub releases..."
